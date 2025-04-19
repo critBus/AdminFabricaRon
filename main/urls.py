8661f7b6d36a677eed2d01728863cebe46d061ae
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-19 19:06:27 -0400

    datos staticos

--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
 admin.site.site_header = 'Ronera'
 admin.site.index_title = 'Administración'
 admin.site.site_title = 'Ronera'
@@ -24,3 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),
 ]
+
+anterior = settings.DEBUG
+settings.DEBUG = True
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if not anterior:
+    settings.DEBUG = False
